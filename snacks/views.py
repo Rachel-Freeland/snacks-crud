eaa1f81eed7400258b9506c3d6b8d1379b61922e
Rachel Freeland <rachel.freeland1@gmail.com>
2022-05-25 00:34:33 -0500

    Scaffolding for lab

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView, UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
+from .models import Snack
 
-# Create your views here.
+
+class SnackListView(ListView):
+    template_name = 'snack_list.html'
+    model = Snack
+
+
+class SnackDetailView(DetailView):
+    template_name = 'snack_detail.html'
+    model = Snack
+
+
+class SnackCreateView(CreateView):
+    template_name = 'snack_create.html'
+
+
+class SnackUpdateView(UpdateView):
+    template_name = 'snack_update.html'
+
+
+class SnackDeleteView(DeleteView):
+    template_name = 'snack_delete.html'
+
